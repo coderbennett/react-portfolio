b9578f9ae7eb2bdeb73ec6b0206f2e580c633019
coderbennett <jbennett@csumb.edu>
2022-05-30 17:27:07 -0700

    add navigation component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 function Navigation() {
     return (
-    ''
+        <div className={`navbar bg-base-100`}>
+            <div className="flex-1">
+                <a className={`btn btn-ghost normal-case text-xl`}>joey's portfolio</a>
+            </div>
+            <div className={`flex-none`}>
+                <ul className={`menu menu-horizontal p-0`}>
+                <li><a>Projects</a></li>
+                <li><a>Resume</a></li>
+                </ul>
+            </div>
+        </div>
     );
 }
 
